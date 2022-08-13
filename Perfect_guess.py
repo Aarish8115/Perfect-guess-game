1703ba5beecd96bae20eb8d547ac4033c344e649
Aarish <mohdaarish127@gmail.com>
2022-08-13 10:49:13 +0530

    renaming variables

--- Perfect_guess.py
+++ Perfect_guess.py
@@ -1,16 +1,16 @@
 import random
 
-a=random.randint(1,100)
+num=random.randint(1,100)
 
-b=int(input("Guess the number between 1-100\n"))
+user_input=int(input("Guess the number between 1-100\n"))
 guesses=0
-while b!=a:
+while user_input!=num:
     guesses+=1
-    if a>b:
-        b=int(input("Enter  a larger number\n"))
-    elif a<b:
-        b=int(input("Enter a smaller number\n"))
-if a==b :
+    if num>user_input:
+        user_input=int(input("Enter  a larger number\n"))
+    elif num<user_input:
+        user_input=int(input("Enter a smaller number\n"))
+if num==user_input :
     guesses+=1
     print("You guessed the right number.")
     print("Number of guesses :",guesses)
