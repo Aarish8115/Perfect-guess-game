3d8eff02419bdd6b859048dbb353dddf9f1d2692
Aarish <mohdaarish127@gmail.com>
2022-08-13 11:10:34 +0530

    Improved with error handling

--- Perfect_guess.py
+++ Perfect_guess.py
@@ -2,9 +2,15 @@ import random
 
 num=random.randint(1,100)
 
-user_input=int(input("Guess the number between 1-100\n"))
+user_input=input("Guess the number between 1-100\n")
 guesses=0
 while user_input!=num:
+    try:
+        user_input=int(user_input)
+    except:
+        print("Please enter a valid value")
+        user_input=input("Enter the number between 1-100\n")
+        continue
     guesses+=1
     if num>user_input:
         user_input=int(input("Enter  a larger number\n"))
